b1c0417bb5841ab81aaba00a84f35c8f64bb6d9e
EnKr99 <JJINGHHUI99.18@ICHAT.SP.EDU.SG>
2020-01-21 22:19:11 +0800

    Added input interface --- JingHui

--- app/src/main/java/mapp/com/sg/myfirstapplication/InputValueScreen.java
+++ app/src/main/java/mapp/com/sg/myfirstapplication/InputValueScreen.java
@@ -1,10 +1,13 @@
 package mapp.com.sg.myfirstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.baoyz.swipemenulistview.SwipeMenuListView;
@@ -21,6 +24,10 @@ public class InputValueScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_interface);
 
+//        getSupportActionBar().setTitle("All your expenses:");
+//        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+
         // Keyboard part
         EditText inputNumberText = (EditText) findViewById(R.id.inputNumber_txt);
         MyKeyboard keyboard = (MyKeyboard) findViewById(R.id.keyboard);
@@ -29,6 +36,19 @@ public class InputValueScreen extends AppCompatActivity {
         inputNumberText.setShowSoftInputOnFocus(false); // disable keyboard
         InputConnection ic = inputNumberText.onCreateInputConnection(new EditorInfo());
         keyboard.setInputConnection(ic);
+
+
+        Button backButton = (Button) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent i = new Intent(new InputValueScreen(), ViewListContents.class); // why this not working
+                Intent i = new Intent(InputValueScreen.this, ViewListContents.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
